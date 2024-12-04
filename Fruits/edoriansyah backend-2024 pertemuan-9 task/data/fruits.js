294aff2bf33d63fc933d802bf8d69650f5be0adc
Mikail Musa <muha23030ti@student.nurulfikri.ac.id>
2024-12-04 14:00:12 +0700

    praktikum 9

---
+++ Fruits/edoriansyah backend-2024 pertemuan-9 task/data/fruits.js
@@ -0,0 +1,9 @@
+/**
+ * TODO 1:
+ * - Buat array yang berisi data buah.
+ * - Refactor variable ke ES6 Variable.
+ */
+let buah = ['jeruk','apel','durian'];
+
+// TODO 2: export variable fruits
+module.exports = buah;
